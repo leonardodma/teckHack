eb4ac20cb232742444e5610517fa758d5fc1f551
leonardodma <leonardodma@al.insper.edu.br>
2022-09-12 15:51:18 -0300

    refector of port scanner

--- roteiro_2/portScanner.py
+++ roteiro_2/portScanner.py
@@ -2,13 +2,25 @@ import socket
 
 ip = input("IP a ser escaneado: ")
 port1 = int(input("Porta 1: "))
-port2 = int(input("Porta 2 (até 65535): "))
-print(f"Começando Scan em {ip}, da porta {port1} até a porta {port2}:\n\n")
+port2 = int(input("Porta 2 (A maior existente é a 65535): "))
+print(f"Começando Scan em {ip}, da porta {port1} até a porta {port2}:\n")
 
-for i in range(port1, port2):
+for port in range(port1, port2):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if s.connect_ex((ip, i)) == 0:
-        service = socket.getservbyport(i, "tcp")
-        print(f"A Porta {i} está aberta, rodando o serviço {service}")
+    if s.connect_ex((ip, port)) == 0:
+        print(f"A Porta {port} está aberta:")
+        try:
+            service_tcp = socket.getservbyport(port, "tcp")
+            print(f"Rodando o Serviço TCP: {service_tcp}")
+        except:
+            pass
+
+        try:
+            service_udp = socket.getservbyport(port, "udp")
+            print(f"Rodando o Serviço UDP: {service_udp}")
+        except:
+            pass
+
+        print("=======================================================")
 
     s.close()
